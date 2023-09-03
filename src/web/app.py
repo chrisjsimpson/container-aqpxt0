708ddfe826a57024d066d68d8f155a7305fa9184
chrisjsimpson <chris15leicester@gmail.com>
2023-09-03 13:48:04 +0100

    ansible vault ui

--- src/web/app.py
+++ src/web/app.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
+import tempfile
+import subprocess
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 
 @app.route("/health")
@@ -30,3 +27,40 @@ def health():
         log.error(msg)
 
     return health
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Retrieve data from the form using request.form
+        secret_name = request.form.get("secretName")
+        vault_password = request.form.get("vaultPassword")
+        secret_value = request.form.get("secretValue")
+        # Save vault_password to a temporary file called 'password' (mktemp)
+        # Run:
+        # ansible-vault encrypt_string \
+        # --vault-password-file password 'secret_value --name 'secret_name'
+        # Save vault_password to a temporary file called 'password' (mktemp)
+        with tempfile.NamedTemporaryFile(
+            mode="w+t", delete=False
+        ) as temp_file:  # noqa: E501
+            temp_file.write(vault_password)
+            temp_file_path = temp_file.name
+
+        cmd = [
+            "ansible-vault",
+            "encrypt_string",
+            "--vault-password-file",
+            temp_file_path,
+            secret_value,
+            "--name",
+            secret_name,
+        ]
+        result = subprocess.run(cmd, capture_output=True, text=True)
+
+        # Deleting the temporary password file
+        subprocess.run(["rm", temp_file_path])
+
+        return "Received data: {}\nEncrypted result: <pre>{}</pre>".format(
+            secret_name, result.stdout
+        )
+    return render_template("index.html")
